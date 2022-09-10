303e7b96c39ca34ae9d83901f8bfb4a2208384c0
gigglebite <candelaria.aleeyajenzen@auf.edu.ph>
2022-09-10 08:26:13 +0800

    Uploaded the files

---
+++ file-uploader/index.php
@@ -0,0 +1,102 @@
+<?php
+
+include "form-handler.php";
+
+$dsn = "mysql:host=localhost;dbname=pdc10_db";
+$user = "admin2";
+$passwd = "root";
+
+$pdo = new PDO($dsn, $user, $passwd);
+
+$result = Registration::handleUpload($_FILES['picture_path']);
+$pass = Registration::encPass($_POST['password'])
+
+?>
+
+<?php
+
+if ($result !== FALSE) {
+
+	// Save the uploaded file to DB. File name as the label
+	$obj = new Registration($_POST['complete_name'],$_POST['email'],$pass['password'],$result['path']);
+	$result = $obj->save();
+  echo ("Successfully saved your registration");
+
+} else {
+  echo ("Unable to process your registration");
+}
+  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </head>
+  <body>
+  <div class="container">
+
+<div class="row">
+<div class="col">
+    <h2>Registrations</h2>
+</div>
+<div class="col">
+<a href="register.php"><button class="btn btn-primary">Add New Registrations</button></a>
+</div>
+
+    <table class="table table-hover">
+
+        <thead>
+
+            <tr>
+
+                <th>ID</th>
+                <th>Complete Name</th>
+                <th>Email</th>
+                <th>Picture</th>
+                <th>Registered Date</th>
+
+            </tr>
+
+        </thead>
+
+
+        <tbody>
+
+            <?php
+                $registrations = Registration::retrieveRegistration();
+                foreach($registrations as $registration)
+
+                {
+
+            ?>
+
+            <tr>
+                <td>
+                    <?php echo $registration['id']; ?>
+                </td>
+
+                <td>
+                    <?php echo $registration['complete_name']; ?>
+                </td>
+                <td>
+                    <?php echo $registration['email']; ?>
+                </td>
+                <td>
+                    <?php 
+                    $filename= $registration['picture_path'];
+                    
+                    echo "<img width=100px; height=100; src=" .  $registration['picture_path'] . ">"; ?>
+                </td>
+                <td>
+                    <?php echo $registration['registered_at']; ?>
+                </td>
+
+                <?php
+
+}
+
+?>
+
+  </body>
+</html>
---
+++ file-uploader/register.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="card mx-auto" style="width: 30rem;">
+  <div class="card-body">
+    <h2 class="card-title">Registration Form</h2>
+    <form method="POST" enctype="multipart/form-data" action="index.php">
+  <div class="mb-3">
+    <label for="fullname" class="form-label">Complete Name</label>
+    <input type="fullname" class="form-control" id="fullname" name="complete_name">
+  </div>
+  <div class="mb-3">
+    <label for="email" class="form-label">Email Address</label>
+    <input type="email" class="form-control" id="email" name="email">
+  </div>
+  <div class="mb-3">
+    <label for="password" class="form-label">Password</label>
+    <input type="password" class="form-control" id="password">
+  </div>
+  <div class="mb-3">
+    <label for="password" class="form-label">Confirm Password</label>
+    <input type="password" class="form-control" id="password" name="password">
+  </div>
+  <div class="mb-3 form-check">
+    <label for="input_file" class="form-label">Picture</label>
+    <input name="picture_path" class="form-control" type="file" id="picture_path">
+  </div>
+  <div class="text-center">
+  <button type="submit" class="btn btn-primary">Submit Registration</button>
+  </form>
+</div>
+  </div>
+</div>
+</body>
+</html>
